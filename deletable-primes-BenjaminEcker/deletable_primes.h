614de92ba871c6daccacb47657aa62ef237bfce0
Benjamin Ecker <43369801+BenjaminEcker@users.noreply.github.com>
2019-02-12 17:00:45 +0100

    Add files via upload

---
+++ deletable-primes-BenjaminEcker/deletable_primes.h
@@ -0,0 +1,23 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Klasse: 2AHDV
+ * ---------------------------------------------------------
+ * Exercise Number: 0
+ * Title:			deletable_primes.h
+ * Author:			P. Bauer
+ * Due Date:		December 1, 2010
+ * ----------------------------------------------------------
+ * Description:
+ * CatCoder Deletable Primes.
+ * ----------------------------------------------------------
+ */
+#ifndef ___DELETABLE_PRIMES
+#define ___DELETABLE_PRIMES
+
+unsigned long remove_digit(int index, unsigned long n);
+
+int get_ways(unsigned long p);
+
+int get_length(unsigned long p);
+
+bool is_prime(unsigned long p);
+#endif
